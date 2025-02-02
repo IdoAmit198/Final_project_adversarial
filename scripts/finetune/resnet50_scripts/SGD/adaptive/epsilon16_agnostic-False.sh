571e19beb36e404d001057fd39f05e1a48046c6e
IdoAmit198 <dodoamit198@gmail.com>
2025-02-02 13:33:04 +0200

    Finetune scripts

---
+++ scripts/finetune/resnet50_scripts/SGD/adaptive/epsilon16_agnostic-False.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source $HOME/miniconda3/etc/profile.d/conda.sh
+conda activate cs236207
+
+# This script requires the sbatch to run with at least 16 CPUs!
+srun -c 16 --gres=gpu:1 python3 run.py --train_method adaptive --model_name resnet50 \
+    --dataset imagenet --Train --Inference --batch_size 64 --fine_tune clean --max_epochs 50
